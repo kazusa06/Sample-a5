48bb89efb57be0f13404994970b6256a16d0964e
kazusa06 <i8w2e3q1@email.com>
2021-10-27 21:55:19 +0900

    square

--- 2つ/main.js
+++ 2つ/main.js
@@ -1,11 +1,10 @@
 window.onload = () => {
   const CANVAS = document.querySelector('canvas');
   const CTX = CANVAS.getContext('2d');
-  const CTX2 = CANVAS.getContext('2d');
 
   CTX.fillStyle = 'red';
   CTX.fillRect(100,100,100,100);
 
-  CTX2.fillStyle = 'black';
-  CTX2.fillRect(300,100,100,100);
+  CTX.fillStyle = 'black';
+  CTX.fillRect(300,100,100,100);
 }
---
+++ ループでたくさん/main.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+  const CANVAS = document.querySelector('canvas');
+  const CTX = CANVAS.getContext('2d');
+
+  let number = 50;      // x座標の変数
+  let number2 = 100;    // y座標の変数
+
+  // for (let i = 1; i < 6; i++) {   // これは５こ作ったやつ。あるとわかりやすいと思ったので残しとく。
+  //   CTX.fillStyle = 'black';
+  //   CTX.fillRect(number,100,50,50);
+  //   number += 100;
+  // }
+
+  for (let i = 1; i < 16; i++) {
+    CTX.fillStyle = 'black';
+    CTX.fillRect(number,number2,50,50);
+    number += 100;
+    if (i % 5 === 0) { // 改行
+      number = 50;
+      number2 += 150;
+    }
+  }
+}
